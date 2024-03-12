fbe6acd4540f1d843bcf6fd84705a1a1a3bde548
Cecilia Sanare <ceci@sanare.dev>
2024-03-12 11:04:28 -0500

    feat: allow specifying wayland support for nvidia

--- nixos/nix-desktop/type/gnome.nix
+++ nixos/nix-desktop/type/gnome.nix
@@ -3,15 +3,17 @@ let
   cfg = config.nix-desktop;
 
   libx = import ../../../lib { inherit config pkgs; };
-  inherit (lib) mkIf mkDefault;
+  inherit (lib) mkIf mkDefault mkEnableOption;
 in
 {
+  options.nix-desktop.nvidia.allow-wayland = mkEnableOption "wayland support on nvidia";
+
   config = mkIf (cfg.enable && libx.isGnome) {
     services = {
       xserver.desktopManager.gnome.enable = true;
       xserver.displayManager.gdm = {
         enable = true;
-        wayland = libx.isNotNvidia;
+        wayland = cfg.nvidia.allow-wayland || libx.isNotNvidia;
       };
     };
   };
