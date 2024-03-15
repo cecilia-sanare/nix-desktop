fde9200aa6afbcb1aa519f27f3e1644a23a457cb
Cecilia Sanare <ceci@sanare.dev>
2024-03-14 20:24:11 -0500

    fix: cursor scaling and blurriness for wayland

--- nixos/nix-desktop/type/gnome.nix
+++ nixos/nix-desktop/type/gnome.nix
@@ -4,6 +4,7 @@ let
 
   libx = import ../../../lib { inherit config pkgs; };
   inherit (lib) mkIf mkDefault mkEnableOption;
+  isWayland = cfg.nvidia.allow-wayland || libx.isNotNvidia;
 in
 {
   options.nix-desktop.nvidia.allow-wayland = mkEnableOption "wayland support on nvidia";
@@ -13,8 +14,18 @@ in
       xserver.desktopManager.gnome.enable = true;
       xserver.displayManager.gdm = {
         enable = true;
-        wayland = cfg.nvidia.allow-wayland || libx.isNotNvidia;
+        wayland = isWayland;
       };
     };
+
+    # This fixes cursor scaling issues on Wayland for Electron
+    environment.sessionVariables.NIXOS_OZONE_WL = mkIf (isWayland) "1";
+
+    programs.dconf.profiles.user.databases = [
+      (mkIf (isWayland) {
+        # VSCode and other apps get all blurry without this when NIXOS_OZONE_WL is set
+        settings."org/gnome/mutter".experimental-features = [ "scale-monitor-framebuffer" ];
+      })
+    ];
   };
 }
